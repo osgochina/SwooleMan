1b005d4faf33bf0750b09b1b21f51ff7d98a8c85
ClownFish <osgochina@gmail.com>
2017-07-17 11:43:45 +0800

    重构

--- Worker.php
+++ Worker.php
@@ -14,6 +14,8 @@ use SwooleMan\Connection\TcpConnection;
 
 class Worker
 {
+
+    const VERSION = '3.4.2';
     /**
      * 当前worker进程的id编号
      * @var int
@@ -199,7 +201,12 @@ class Worker
     /**
      * @var \Swoole\Server;
      */
-    protected static $swServer;
+    protected  $swServer;
+    /**
+     * worker 的实例
+     * @var Worker;
+     */
+    protected static $_instance = null;
 
     protected static $_builtinTransports = array(
         'tcp'   => 'tcp',
@@ -216,27 +223,71 @@ class Worker
      */
     protected static $_startFile = '';
 
+    /**
+     * The file to store status info of current worker process.
+     *
+     * @var string
+     */
+    protected static $_statisticsFile = '';
+
+    /**
+     * Maximum length of the worker names.
+     *
+     * @var int
+     */
+    protected static $_maxWorkerNameLength = 12;
+
+    /**
+     * Maximum length of the socket names.
+     *
+     * @var int
+     */
+    protected static $_maxSocketNameLength = 12;
+
+    /**
+     * Maximum length of the process user names.
+     *
+     * @var int
+     */
+    protected static $_maxUserNameLength = 12;
+
 
 
     public function __construct($listen,$context = '')
     {
         $this->_socketName = $listen;
+        $this->_init();
         $this->_paramSocketName($this->_socketName);
         $this->_createSetting();
-        $this->_newService();
+        if (self::$_instance){
+            throw new \Exception("worker 对象已存在");
+        }
+        self::$_instance = $this;
     }
 
     public static function runAll()
     {
         self::_checkSapiEnv();
-        self::_init();
         self::_parseCommand();
         self::_run();
     }
 
     public static function stopAll()
     {
-
+        // For master process.
+        $master_pid = self::$_instance->swServer->master_pid;
+        if ( $master_pid === posix_getpid()) {
+            self::log("SwooleMan[" . basename(self::$_startFile) . "] Stopping ...");
+            posix_kill($master_pid, SIGTERM);
+            // Remove statistics file.
+            if (is_file(self::$_statisticsFile)) {
+                @unlink(self::$_statisticsFile);
+            }
+        } // For child processes.
+        else {
+            self::$_instance->swServer->stop();
+            // Execute exit.
+        }
     }
 
     public static function listen()
@@ -318,11 +369,13 @@ class Worker
         $this->_setting['enable_reuse_port'] = $this->reusePort;
         //守护进程运行
         $this->_setting['daemonize'] = self::$daemonize;
+
+        self::$_statisticsFile                      = sys_get_temp_dir() . '/swooleman.status';
     }
 
     public function _onStart($server)
     {
-
+        $this->displayUI($server);
     }
     public function _onShutdown($server)
     {
@@ -494,12 +547,51 @@ class Worker
 
     public function _onOpen($server, $req)
     {
-
+        $fd = $req->fd;
+        $connection                         = new TcpConnection($server, $fd);
+        $this->connections[$fd] = $connection;
+        $connection->worker                 = $this;
+        $connection->protocol               = $this->protocol;
+        $connection->transport              = $this->transport;
+        $connection->onMessage              = $this->onMessage;
+        $connection->onClose                = $this->onClose;
+        $connection->onError                = $this->onError;
+        $connection->onBufferDrain          = $this->onBufferDrain;
+        $connection->onBufferFull           = $this->onBufferFull;
+        // Try to emit onConnect callback.
+        if ($this->onConnect) {
+            try {
+                call_user_func($this->onConnect, $connection);
+            } catch (\Exception $e) {
+                self::log($e);
+                exit(250);
+            } catch (\Error $e) {
+                self::log($e);
+                exit(250);
+            }
+        }
     }
 
     public function _onMessage($server,$frame)
     {
-
+        $fd = $frame->fd;
+        if (!isset($this->connections[$fd])){
+            return;
+        }
+        $connection = $this->connections[$fd];
+        ConnectionInterface::$statistics['total_request']++;
+        // Try to emit onConnect callback.
+        if ($connection->onMessage) {
+            try {
+                call_user_func($connection->onMessage, $connection,$frame->data);
+            } catch (\Exception $e) {
+                self::log($e);
+                exit(250);
+            } catch (\Error $e) {
+                self::log($e);
+                exit(250);
+            }
+        }
     }
 
     /**
@@ -619,19 +711,19 @@ class Worker
     {
         switch (strtolower($this->transport)){
             case "tcp":
-                self::$swServer = $this->_newTcpServer();
+                $this->swServer = $this->_newTcpServer();
                 break;
             case "udp":
-                self::$swServer = $this->_newUdpServer();
+                $this->swServer = $this->_newUdpServer();
                 break;
             case "unix":
-                self::$swServer = $this->_newUnixServer();
+                $this->swServer = $this->_newUnixServer();
                 break;
             case "http":
-                self::$swServer = $this->_newHttpServer();
+                $this->swServer = $this->_newHttpServer();
                 break;
             case "websocket":
-                self::$swServer = $this->_newWebSocketServer();
+                $this->swServer = $this->_newWebSocketServer();
                 break;
         }
     }
@@ -671,21 +763,177 @@ class Worker
         }
     }
 
-    protected static function _init()
+    protected  function _init()
     {
+        // Worker name.
+        if (empty($this->name)) {
+            $this->name = 'none';
+        }
+        // Get maximum length of worker name.
+        $worker_name_length = strlen($this->name);
+        if (self::$_maxWorkerNameLength < $worker_name_length) {
+            self::$_maxWorkerNameLength = $worker_name_length;
+        }
+        // Get maximum length of socket name.
+        $socket_name_length = strlen($this->_socketName);
+        if (self::$_maxSocketNameLength < $socket_name_length) {
+            self::$_maxSocketNameLength = $socket_name_length;
+        }
 
+        // Get unix user of the worker process.
+        if (empty($this->user)) {
+            $this->user = self::getCurrentUser();
+        } else {
+            if (posix_getuid() !== 0 && $this->user != self::getCurrentUser()) {
+                self::log('Warning: You must have the root privileges to change uid and gid.');
+            }
+        }
+        // Get maximum length of unix user name.
+        $user_name_length = strlen($this->user);
+        if (self::$_maxUserNameLength < $user_name_length) {
+            self::$_maxUserNameLength = $user_name_length;
+        }
     }
 
     protected static function _parseCommand()
     {
 
+        global $argv;
+        // Check argv;
+        $start_file = $argv[0];
+        if (!isset($argv[1])) {
+            exit("Usage: php yourfile.php {start|stop|restart|reload|status}\n");
+        }
+
+        // Get command.
+        $command  = trim($argv[1]);
+        $command2 = isset($argv[2]) ? $argv[2] : '';
+
+        // Start command.
+        $mode = '';
+        if ($command === 'start') {
+            if ($command2 === '-d' || Worker::$daemonize) {
+                $mode = 'in DAEMON mode';
+            } else {
+                $mode = 'in DEBUG mode';
+            }
+        }
+        self::log("SwooleMan[$start_file] $command $mode");
+
+        // Get master process PID.
+        $master_pid      = @file_get_contents(self::$pidFile);
+        $master_is_alive = $master_pid && @posix_kill($master_pid, 0);
+        // Master is still alive?
+        if ($master_is_alive) {
+            if ($command === 'start' && posix_getpid() != $master_pid) {
+                self::log("SwooleMan[$start_file] already running");
+                exit;
+            }
+        } elseif ($command !== 'start' && $command !== 'restart') {
+            self::log("SwooleMan[$start_file] not run");
+            exit;
+        }
+
+        // execute command.
+        switch ($command) {
+            case 'start':
+                if ($command2 === '-d') {
+                    Worker::$daemonize = true;
+                }
+                break;
+            case 'status':
+//                if (is_file(self::$_statisticsFile)) {
+//                    @unlink(self::$_statisticsFile);
+//                }
+//                // Master process will send status signal to all child processes.
+//                posix_kill($master_pid, SIGUSR2);
+//                // Waiting amoment.
+//                usleep(500000);
+//                // Display statisitcs data from a disk file.
+//                @readfile(self::$_statisticsFile);
+                exit(0);
+            case 'stop':
+                self::log("SwooleMan[$start_file] is stoping ...");
+                // Send stop signal to master process.
+                $master_pid && posix_kill($master_pid, SIGTERM);
+                // Timeout.
+                $timeout    = 5;
+                $start_time = time();
+                // Check master process is still alive?
+                while (1) {
+                    $master_is_alive = $master_pid && posix_kill($master_pid, 0);
+                    if ($master_is_alive) {
+                        // Timeout?
+                        if (time() - $start_time >= $timeout) {
+                            self::log("SwooleMan[$start_file] stop fail");
+                            exit;
+                        }
+                        // Waiting amoment.
+                        usleep(10000);
+                        continue;
+                    }
+                    // Stop success.
+                    self::log("SwooleMan[$start_file] stop success");
+                    if ($command === 'stop') {
+                        exit(0);
+                    }
+                    if ($command2 === '-d') {
+                        Worker::$daemonize = true;
+                    }
+                    break;
+                }
+                break;
+            case 'reload':
+                posix_kill($master_pid, SIGUSR1);
+                self::log("SwooleMan[$start_file] reload");
+                exit;
+            default :
+                exit("Usage: php yourfile.php {start|stop|restart|reload|status}\n");
+        }
     }
 
 
 
     protected static function _run()
     {
-        self::$swServer->start();
+        self::$_instance->_newService();
+        self::$_instance->swServer->start();
+    }
+
+    protected static function getCurrentUser()
+    {
+        $user_info = posix_getpwuid(posix_getuid());
+        return $user_info['name'];
+    }
+
+
+    /**
+     * Display staring UI.
+     *
+     * @return void
+     */
+    protected  function displayUI($server)
+    {
+        self::safeEcho("\033[1A\n\033[K-----------------------\033[47;30m SWOOMEMAN \033[0m-----------------------------\n\033[0m");
+        self::safeEcho('SwooleMan version:'. Worker::VERSION. "          PHP version:". PHP_VERSION. "\n");
+        self::safeEcho("------------------------\033[47;30m WORKERS \033[0m-------------------------------\n");
+        self::safeEcho("\033[47;30muser\033[0m". str_pad('',
+                self::$_maxUserNameLength + 2 - strlen('user')). "\033[47;30mworker\033[0m". str_pad('',
+                self::$_maxWorkerNameLength + 2 - strlen('worker')). "\033[47;30mlisten\033[0m". str_pad('',
+                self::$_maxSocketNameLength + 2 - strlen('listen')). "\033[47;30mprocesses\033[0m \033[47;30m". "status\033[0m\n");
+
+        self::safeEcho(str_pad($this->user, self::$_maxUserNameLength + 2). str_pad($this->name,
+                self::$_maxWorkerNameLength + 2). str_pad($this->_socketName,
+                self::$_maxSocketNameLength + 2). str_pad(' ' . $this->count, 9). " \033[32;40m [OK] \033[0m\n");
+
+        self::safeEcho("----------------------------------------------------------------\n");
+        if (self::$daemonize) {
+            global $argv;
+            $start_file = $argv[0];
+            self::safeEcho("Input \"php $start_file stop\" to quit. Start success.\n\n");
+        } else {
+            self::safeEcho("Press Ctrl-C to quit. Start success.\n");
+        }
     }
 
 }
